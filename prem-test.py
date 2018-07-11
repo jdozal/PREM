0c96f6a59ac940e7f2981ebc9c6acf608b0bd4e5
Jessica Dozal <jldozalcruz@miners.utep.edu>
2018-07-11 10:14:59 -0600

    Updated Script

--- prem-test.py
+++ prem-test.py
@@ -10,17 +10,34 @@ import pandas as pd
 prem = pd.read_csv('PREM500.csv', names=['radius(m)','density(kg/m^3)','Vpv(m/s)','Vsv(m/s)','Q-kappa','Q-mu','Vph(m/s)','Vsh(m/s)','eta'])
 
 g = []
+# Radius of earth in km
 R = 6371
 
 rad = prem["radius(m)"]/1000
 prem["radius(km)"] = rad
+
 # calculating gravity at depth d
 for index, row in prem.iterrows():
     currG = 9.81*(1-(row["radius(km)"]/R))
     g.append(currG)
 
 # calculating gravity at depth d, shorter version, less decimals
-testG = 9.81*(1-(prem["density(kg/m^3)"]/prem["radius(m)"]))
+testG = 9.81*(1-(prem["radius(km)"]/R))
 
 # adding column g to PREM
-prem["g"] = g
+prem["g"] = testG
+
+# pv = mass 
+pv = prem["density(kg/m^3)"]*100*100*(6371-prem["radius(km)"])
+
+# column for Gravitational Potential Energy 
+E=prem["density(kg/m^3)"]*prem["g"]*prem["radius(m)"]
+
+# adding E to prem
+prem["E"] = E
+
+# Keeping only used columns 
+prem2 = prem.drop(['Vpv(m/s)','Vsv(m/s)','Q-kappa','Q-mu','Vph(m/s)','Vsh(m/s)','eta'], axis=1)
+
+# Creating cvs file 
+prem2.to_csv('PREM_Edit.csv')
